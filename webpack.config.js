6c6b701dc6384b80d19db568095238a26343a16f
Dacre <enquiries@mooce.co.nz>
2018-11-04 23:16:53 +1300

    added html webpack plugin for index gen

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -19,6 +20,9 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
   resolve: {
     extensions: ['.js']
   }

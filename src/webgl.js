642251e37c6b4331dc9abd6dc04198ab42b6d0cc
Dacre <enquiries@mooce.co.nz>
2018-11-12 21:56:01 +1300

    adding webgl accelerated mandelbrot

--- src/webgl.js
+++ src/webgl.js
@@ -62,10 +62,45 @@ const initShader = (gl) => {
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, `
     precision highp float;
     
-    varying vec2 vUV; 
+    varying vec2 vUV;  
+
+    vec4 getColor(float phase) {
+        
+        return vec4(
+            (cos(phase) + 1.0) * 0.5,
+            (sin(phase + 3.1415) + 1.0) * 0.5,
+            (sin(phase) + 1.0) * 0.5,
+            1.0
+        );
+    }
 
     void main() {
-        gl_FragColor = vec4(vUV,0.0,1.0);
+
+        vec4 color = vec4(0.0,0.0,0.0,1.0); 
+
+        float COMPx = 0.0;
+        float COMPy = 0.0;
+        float z = 0.0;
+
+        float x = vUV.x;
+        float y = vUV.y;
+
+        for(float i = 0.0; i < 1.0; i += (1.0 / 100.0)) {
+            
+            float COMPx_new = COMPx * COMPx - COMPy * COMPy + x;
+            float COMPy_new = 2.0 * COMPx * COMPy + y;
+            float zN = (COMPx_new + COMPy_new);
+
+            if (abs(zN - z) > 5.0) {
+                color = getColor(i * 10.0 + 0.0);
+            }
+
+            COMPx = COMPx_new;
+            COMPy = COMPy_new;
+            z = zN;
+        }
+
+        gl_FragColor = color;
     }
 `);
 

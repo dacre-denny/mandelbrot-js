22649e9fc78694ea23e81be70624fad83e5578ab
Dacre <enquiries@mooce.co.nz>
2018-11-24 23:07:21 +1300

    added valid element checks and slider value display to ui module

--- src/ui.js
+++ src/ui.js
@@ -2,29 +2,47 @@ export const createSlider = (id, value, onChange) => {
 
     const element = document.getElementById(id)
 
+    if (!element) return
+
+    const updateValueDisplay = (text) => {
+
+        element.parentElement.querySelector('span').innerText = text
+    }
+
     element.value = value
 
-    element.addEventListener('change', onChange)
+    element.addEventListener('change', (event) => {
+
+        updateValueDisplay(event.currentTarget.value)
+
+        onChange(event)
+    })
+
+    updateValueDisplay(value)
 }
 
 export const createToggle = (id, value, onToggle) => {
 
     const element = document.getElementById(id)
 
+    if (!element) return
+
     element.classList.toggle('toggled', value)
 
-    element.addEventListener('click', () => {
+    element.addEventListener('click', (event) => {
 
         value = !value
         element.classList.toggle('toggled', value)
 
-        onToggle()
+        onToggle(event)
     })
 }
 
 export const createButton = (id, onClick) => {
 
     const element = document.getElementById(id)
+
+    if (!element) return
 
     element.addEventListener('click', onClick)
 }

9a777279c0555639aad5a22a250309a885ce3a43
Dacre <enquiries@mooce.co.nz>
2018-11-29 23:04:15 +1300

    mocha configured for es5/esnext syntax, added jsdom for dom mocking test support added tests for UI slider method

---
+++ test/ui.spec.js
@@ -0,0 +1,61 @@
+import {
+  assert
+} from 'chai';
+import * as UI from '../src/ui'
+
+const jsdom = require("jsdom");
+const {
+  JSDOM
+} = jsdom;
+
+
+describe('createSlider', function () {
+
+  describe('behaviour when invalid id passed', function () {
+
+    it('should do nothing when invalid element id passed', function () {
+
+      UI.createSlider()
+    });
+
+    it('should do nothing when no matching element found for id', function () {
+
+      const document = new JSDOM().window.document
+      const div = document.createElement(`div`)
+
+      document.body.appendChild(div)
+      global.document = document
+
+      div.innerHTML = `
+    <label>Bar</label>
+    <span></span>
+    <input type="range" min="1" max="500" step="1" id="bar" />
+    `
+
+      UI.createSlider('foo')
+    });
+
+  });
+
+  describe('behaviour when id matches element in document', function () {
+
+    it('should update range input with value', function () {
+
+      const document = new JSDOM().window.document
+      const div = document.createElement(`div`)
+
+      document.body.appendChild(div)
+      global.document = document
+
+      div.innerHTML = `
+      <label>Bar</label>
+      <span></span>
+      <input type="range" min="1" max="500" step="1" id="bar" />
+      `
+
+      assert.isUndefined(UI.createSlider('bar', 4))
+
+      assert.equal(document.getElementById('bar').value, 4)
+    });
+  });
+});
